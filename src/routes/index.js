929ed329e7dfc11c9ba02a0f8f33f16abd2c0923
Tony Xiao <tonyx.ca@gmail.com>
2016-04-29 22:43:35 -0700

    Removing counter and home classes which are not needed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 // We only need to import the modules necessary for initial render
 import CoreLayout from '../layouts/CoreLayout/CoreLayout'
-import Home from './Home'
 
 export const createRoutes = (store) => {
 /*  Note: Instead of using JSX, we are using react-router PlainRoute,
@@ -10,7 +9,6 @@ export const createRoutes = (store) => {
   const routes = {
     path: '/',
     component: CoreLayout,
-    indexRoute: Home,
     getChildRoutes (location, next) {
       require.ensure([], (require) => {
         next(null, [
