2234293ab057c67fa4a5aa0b96b0889b600f2574
Tony Xiao <tonyx.ca@gmail.com>
2016-04-29 19:03:25 -0700

    Populate the rest of the API for the various API methods to test against

--- server/api.js
+++ server/api.js
@@ -3,13 +3,40 @@ import route from 'koa-route'
 import jsonBody from 'koa-json-body'
 
 const segment = (writeKey) => Analytics(writeKey, { flushAt: 1 })
+const throwJson = (ctx, err, status = 400) => {
+  ctx.throw(JSON.stringify({error: err.message || 'Unknown Error'}, null, 4), status)
+}
 
 export default function (app) {
   app.use(jsonBody())
   app.use(route.post('/api/identify/:writeKey', (ctx, writeKey) => {
-    segment(writeKey).identify(ctx.request.body)
-    ctx.body = {
-      status: 'success'
-    }
+    try {
+      segment(writeKey).identify(ctx.request.body)
+    } catch (err) { throwJson(ctx, err) }
+    ctx.body = {status: 'success'}
+  }))
+  app.use(route.post('/api/track/:writeKey', (ctx, writeKey) => {
+    try {
+      segment(writeKey).track(ctx.request.body)
+    } catch (err) { throwJson(ctx, err) }
+    ctx.body = {status: 'success'}
+  }))
+  app.use(route.post('/api/page/:writeKey', (ctx, writeKey) => {
+    try {
+      segment(writeKey).page(ctx.request.body)
+    } catch (err) { throwJson(ctx, err) }
+    ctx.body = {status: 'success'}
+  }))
+  app.use(route.post('/api/alias/:writeKey', (ctx, writeKey) => {
+    try {
+      segment(writeKey).alias(ctx.request.body)
+    } catch (err) { throwJson(ctx, err) }
+    ctx.body = {status: 'success'}
+  }))
+  app.use(route.post('/api/group/:writeKey', (ctx, writeKey) => {
+    try {
+      segment(writeKey).group(ctx.request.body)
+    } catch (err) { throwJson(ctx, err) }
+    ctx.body = {status: 'success'}
   }))
 }
